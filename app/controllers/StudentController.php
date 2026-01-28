0acc66e763900aa4a526b41e6cc68affaa5e7e5a
unknown <vincentwilliam2009@gmail.com>
2026-01-28 09:48:24 +0700

    feat: implement simple router

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controllers;
+
+class StudentController{
+
+    public function index(){
+        echo '<h1>Daftar Siwa</h1>';
+        echo '<p>Menampilkan Daftar Siswa</p>';
+    }
+
+    public function create(){
+        echo '<h1>Tambah Siswa</h1>';
+        echo '<p>Menampilkan Form Tambah Siswa</p>'; 
+
+    }
+}
+?>
--- app/core/Router.php
+++ app/core/Router.php
@@ -1,12 +1,34 @@
 <?php
 namespace App\Core;
 
+use App\Controllers\StudentController;
+
 class Router
 {
 
     public function run()
     {
-        echo 'Router is running';
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+        if ($method == 'GET' && $uri == '/students'){
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller -> index();
+            return;
+        }
+
+        if ($method == 'GET' && $uri == '/students/create'){
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller -> create();
+            return;
+        }
+        
+
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
+
     }
 
 }
